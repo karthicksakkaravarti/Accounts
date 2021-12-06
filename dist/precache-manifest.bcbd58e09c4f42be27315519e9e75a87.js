4918553d820e7d8101241d8152c9d357be50f55d
Karthick Sakkaravarthi Vellaisamy <Karthick.V@radisys.com>
2021-12-06 23:25:13 +0530

    Code changes

---
+++ dist/precache-manifest.bcbd58e09c4f42be27315519e9e75a87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb0144a407b7e0d2055",
+    "url": "/accounts/static/css/chunk-vendors.a37b1574.css"
+  },
+  {
+    "revision": "4ecff378038f94699686",
+    "url": "/accounts/static/css/demo.3d693859.css"
+  },
+  {
+    "revision": "1372cb3ab875aa1dd369",
+    "url": "/accounts/static/js/app.0da7dc78.js"
+  },
+  {
+    "revision": "7bb0144a407b7e0d2055",
+    "url": "/accounts/static/js/chunk-vendors.aa4d9fa6.js"
+  },
+  {
+    "revision": "4ecff378038f94699686",
+    "url": "/accounts/static/js/demo.f84c96ce.js"
+  },
+  {
+    "revision": "d34a0d7444f369964177457a30371fb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97872bc9f46d46bea0550b261db1e23e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
